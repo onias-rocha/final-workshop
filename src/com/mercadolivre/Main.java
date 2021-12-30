720000907b13e0146b91a0b67a39f9817a74ae18
Onias da Rocha <onias.rocha@mercadolivre.com>
2021-12-30 11:35:45 -0400

    final commit

---
+++ src/com/mercadolivre/Main.java
@@ -0,0 +1,67 @@
+package com.mercadolivre;
+
+import static com.mercadolivre.Examples.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    /** Java 14 **/
+
+    //Switch Expressions
+//        var team = testSwitch("Russel Wilson");
+//        System.out.println(team);
+
+
+    //Pattern Matching for instanceof
+//        Pessoa onias = new Pessoa();
+//        onias.nome = "Onias da Rocha Filho";
+//        testInstanceof(onias);
+
+    //Records
+//        record Dog (String breed, String name){};
+//        var scooby = new Dog("German Shepherd", "Scooby Dooby Doo");
+//        System.out.println(scooby.toString());
+
+
+    //Helpful NullPointerExceptions
+//    testNullPointerException();
+
+
+    /** Java 15 **/
+//    Melhorias em Records
+    //Records
+//        record User (String name, int age){
+//            public User {
+//                if (age<18){
+//                    throw new IllegalArgumentException("Sorry " + name + ", you're not old enough to get into the club ");
+//                }
+//            }
+//        };
+//        var newUser = new User("Onias da Rocha", 16);
+//        System.out.println(newUser.toString());
+
+    //Sealed Classes
+//        testSealedClasses();
+
+    //Text Blocks
+//        testTextBlocks();
+
+    //Pattern Matchinig Type Checks
+//        Pessoa onias = new Pessoa();
+//        onias.nome = "Onias da Rocha Filho";
+//        onias.idade = 19;
+//        testInstanceofJava15(onias);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
